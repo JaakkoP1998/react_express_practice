11889dc36c6e08cd9c5c7ee5c2edb84a55d0a2a8
Jaakko Palm <jaakkop1998@gmail.com>
2025-10-25 13:03:01 +0300

    Modified working for my application and updated ReadMe

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # JavaScript + Express
 
-Some basic Express-server testing.
+Some Express-server testing.
+
+Works as a temporary back-end for my react testing.
+https://github.com/JaakkoP1998/react_harjoittelua for front-end.
--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
 
 app.use(express.json())
+app.use(cors())
 
 // Some json-notes for testing
 let notes = [  
     {    
         id: "1",    
-        content: "HTML is easy",    
-        important: true  
+        content: "These are comments meant for testing web-application",    
     },  
     {    
         id: "2",    
-        content: "Browser can execute only JavaScript",    
-        important: false  
+        content: "Some features may not be yet applied",    
     },  
     {    
         id: "3",    
-        content: "GET and POST are the most important methods of HTTP protocol",    
-        important: true  
+        content: "Works as a back-end for https://github.com/JaakkoP1998/react_harjoittelua",    
     }
 ]
 
@@ -28,14 +28,14 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-// "http://localhost:3001/api/notes" to see json-data.
-app.get('/api/notes', (request, response) => {
+// "http://localhost:3001/api/comments/" to see json-data.
+app.get('/api/comments', (request, response) => {
   response.json(notes)
 })
 
 // Get note by id.
 // For example "http://localhost:3001/api/notes/1" shows note matching id number 1. 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/comments/:id', (request, response) => {
   const id = request.params.id
   const note = notes.find(note => note.id === id)
 
@@ -56,7 +56,7 @@ const generateId = () => {
 }
 
 // Method for adding new notes.
-app.post('/api/notes', (request, response) => {  
+app.post('/api/comments', (request, response) => {  
     const body = request.body
 
     // Content-field is not allowed to be empty.
@@ -68,7 +68,6 @@ app.post('/api/notes', (request, response) => {
     
     const note = {
         content: body.content,
-        important: body.important || false,
         id: generateId(),
     }
 
