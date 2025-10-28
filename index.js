0da9143c05cd0a41a890cdd29cd68516be252e36
Jaakko Palm <jaakkop1998@gmail.com>
2025-10-28 12:50:58 +0200

    Modified to use Render web-service

--- index.js
+++ index.js
@@ -85,6 +85,8 @@ app.listen(PORT, () => {
 */
 
 // This part is for running server in Render: https://render.com/
+// Check to see that the server is working:
+// https://react-express-practice.onrender.com/api/comments
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
